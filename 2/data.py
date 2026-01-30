077912dfada5bcadd5a7b9e072e665d13de1fccd
Pravdyvy <awxrvtb@gmail.com>
2026-01-30 09:44:44 +0200

    fix: variance fix

--- 2/data.py
+++ 2/data.py
@@ -6,7 +6,3 @@ def sample_norm_dist(mean, var, num):
 
 def sample_chi_square(free_deg, num):
     return np.random.chisquare(free_deg, num)
-
-import est_std_lib
-
-print(est_std_lib.variance(sample_norm_dist(0, 10, 10000)))
--- 2/est_std_lib.py
+++ 2/est_std_lib.py
@@ -18,8 +18,8 @@ def variance(x):
     x_squared = [(i - mean_x) ** 2 for i in x]
     return sum(x_squared)/len(x)
 
-def variance_wrong(x):
+def variance_short(x):
     mean_x = mean(x)
     x_squared = [i ** 2 for i in x]
     meax_x_squared = mean(x_squared)
-    return (meax_x_squared - mean_x**2)/len(x)
+    return meax_x_squared - mean_x**2
